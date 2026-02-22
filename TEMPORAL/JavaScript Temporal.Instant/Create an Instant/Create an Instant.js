f63d2c90be0bc6b84e049cca3bd7ff1feae6ab4b
Samiran Sarkar <samiransarka123@gmail.com>
2026-02-22 20:30:20 +0530

    temporal.instant

--- TEMPORAL/JavaScript Temporal.Instant/Create an Instant/Create an Instant.js
+++ TEMPORAL/JavaScript Temporal.Instant/Create an Instant/Create an Instant.js
@@ -4,4 +4,7 @@ const now = Temporal.Now.instant();
 console.log(now.toString()); 
 
 // This returns the exact current moment in UTC.
+// 2026-02-22T14:57:28.075Z
+// 2026-02-22T14:58:34.269314253Z
+// 1771772248075
 
