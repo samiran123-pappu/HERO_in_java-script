b512ced8d5b1e593eadbe33e7bc3b0b9bd289ea0
Samiran Sarkar <samiransarka123@gmail.com>
2026-02-22 23:50:02 +0530

    When to Use PlainDate2

---
+++ TEMPORAL/JavaScript Temporal.PlainDate/The Temporal.PlainDate Object/The Temporal.PlainDate Object.js
@@ -0,0 +1,4 @@
+import { Temporal } from "@js-temporal/polyfill";
+
+const date = new Temporal.PlainDate(2026, 5, 17);
+console.log(`${date}`); // Output: 2026-05-17
--- a.js
+++ a.js
@@ -1,8 +1,4 @@
 import { Temporal } from "@js-temporal/polyfill";
 
-let d = new Date(2026, 0, 17); // Month 0 = January
-console.log(d); // Output: Sat Jan 17 2026 00:00:00 GMT+0000 (Coordinated Universal Time)
-
-
-let d2 = Temporal.PlainDate.from('2026-01-17');
-console.log(`${d2}`); // Output: 2026-01-17
+let a = Temporal.Now.plainDateISO();
+console.log(`${a}`); // Output: current date in ISO format (e.g., 2024-06-17)
