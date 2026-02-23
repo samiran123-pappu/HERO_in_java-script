3e765076159188baee750f38cb00435e3a69f27e
Samiran Sarkar <samiransarka123@gmail.com>
2026-02-24 04:10:42 +0530

    // ZonedDateTime handles daylight saving time (DST) safely.2

--- TEMPORAL/JS Temporal Date Arithmetic/Date Arithmetic with ZonedDateTime/Date Arithmetic with ZonedDateTime.js
+++ TEMPORAL/JS Temporal Date Arithmetic/Date Arithmetic with ZonedDateTime/Date Arithmetic with ZonedDateTime.js
@@ -5,6 +5,10 @@ import { Temporal } from "@js-temporal/polyfill";
 
 // Create a ZonedDateTime object
 const date = Temporal.ZonedDateTime.from("2026-05-17T12:00:00Z[America/New_York]");
+const date1 = Temporal.Now.zonedDateTimeISO("America/New_York");
+
+console.log(date.toString());
+console.log(date1.toString());
 
 // Add one day
 const nextDay = date.add({ days: 1 });
