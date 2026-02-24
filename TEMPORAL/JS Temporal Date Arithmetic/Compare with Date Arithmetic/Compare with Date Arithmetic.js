0e576892b1659b4e9fd6cb5b62d4eedd4da51535
Samiran Sarkar <samiransarka123@gmail.com>
2026-02-24 08:39:04 +0530

    Compare with Date Arithmetic

--- TEMPORAL/JS Temporal Date Arithmetic/Compare with Date Arithmetic/Compare with Date Arithmetic.js
+++ TEMPORAL/JS Temporal Date Arithmetic/Compare with Date Arithmetic/Compare with Date Arithmetic.js
@@ -1,7 +1,7 @@
 // Compare with Date Arithmetic
 // Date modifies the original object and may cause DST-related issues.
 
-const start = new Date("2026-02-17");
+const start = new Date("2026-02-7");
 
 start.setDate(start.getDate() + 10);
 
