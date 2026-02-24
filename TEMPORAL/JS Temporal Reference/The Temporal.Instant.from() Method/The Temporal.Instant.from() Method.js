230745234d20aa4e7e5bf8cf1f0f34cc3ab81f8c
Samiran Sarkar <samiransarka123@gmail.com>
2026-02-24 20:10:23 +0530

    Temporal PlainDate Objects

--- TEMPORAL/JS Temporal Reference/The Temporal.Instant.from() Method/The Temporal.Instant.from() Method.js
+++ TEMPORAL/JS Temporal Reference/The Temporal.Instant.from() Method/The Temporal.Instant.from() Method.js
@@ -6,3 +6,9 @@ console.log("1", instant.toString()); // "2026-05-01T12:00:00Z"
 // Using a different string format
 instant = Temporal.Instant.from("2026-05-01T12:00:00.000Z");
 console.log("2", instant.toString()); // "2026-05-01T12:00:00Z"
+
+
+// To get human-readable components like year, month, or hour, you must explicitly convert it to a Temporal.ZonedDateTime using a specific time zone.
+
+const zonedDateTime = Temporal.ZonedDateTime.from(instant.toString() + "[UTC]");
+console.log("3", zonedDateTime.year);
