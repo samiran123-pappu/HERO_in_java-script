3d386c7ced7dcad04f90d0d8b0e8d00e0b524121
Samiran Sarkar <samiransarka123@gmail.com>
2026-02-26 06:36:58 +0530

    JavaScript Arrays

---
+++ JavaScript Arrays/JavaScript Arrays/The length Property/The length Property.js
@@ -0,0 +1,9 @@
+// The length Property
+// The length property of an array returns the length of an array (the number of array elements).
+
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.length);
+
+// The length property is always one more than the highest array index.
+
