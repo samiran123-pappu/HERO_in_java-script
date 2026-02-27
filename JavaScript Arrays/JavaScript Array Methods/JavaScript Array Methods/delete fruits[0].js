e15207aa52aa11638d2574434211f6137a29a265
Samiran Sarkar <samiransarka123@gmail.com>
2026-02-27 11:27:09 +0530

    ji

---
+++ JavaScript Arrays/JavaScript Array Methods/JavaScript Array Methods/delete fruits[0].js
@@ -0,0 +1,9 @@
+// JavaScript Array delete()
+// Warning !
+// Using delete() leaves undefined holes in the array.
+
+// Use pop() or shift() instead.
+
+// Example:
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+delete fruits[0];
